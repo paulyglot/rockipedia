08b99d8736cac4d792c8ce49c757b9f54b9f0f48
Paul Brower <pabrower@icloud.com>
2018-11-03 20:28:12 -0700

    WIP deploy to production

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const routeConfig = require("./config/route-config.js");
+const appConfig = require("./config/main-config.js");
 
+appConfig.init();
 routeConfig.init(app);
 
 module.exports = app;
