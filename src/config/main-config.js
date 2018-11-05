1eb1b5cd4d6bf7dd10844367afd745d5c3568be5
Paul Brower <pabrower@icloud.com>
2018-11-04 20:52:15 -0800

    setup routing and controllers

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
+const bodyParser = require('body-parser');
 const logger = require('morgan');
+const path = require("path");
+const viewsFolder = path.join(__dirname, "..", "views");
 
 module.exports = {
-  init(){
+  init(app, express){
+    app.set("views", viewsFolder);
+    app.set("view engine", "ejs");
+    app.use(express.static(path.join(__dirname, "..", "assets")));
     app.use(logger('dev'));
-    app.use(bodyParser.urlencoded({ extended: true }));
   }
 };
