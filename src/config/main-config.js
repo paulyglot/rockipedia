ce8ced7bf84fa9cf98ee2b5715b77a44972bd7f0
Paul Brower <pabrower@icloud.com>
2018-11-05 00:02:23 -0800

    started validations setup and fixed bug in head.ejs

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const logger = require('morgan');
 const path = require("path");
 const viewsFolder = path.join(__dirname, "..", "views");
+const expressValidator = require("express-validator");
 
 module.exports = {
   init(app, express){
@@ -10,5 +11,6 @@ module.exports = {
     app.set("view engine", "ejs");
     app.use(express.static(path.join(__dirname, "..", "assets")));
     app.use(logger('dev'));
+    app.use(expressValidator());
   }
 };
