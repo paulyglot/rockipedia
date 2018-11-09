a32ed65dea86515f0c3a7e0e2e32f555e6ce8237
Paul Brower <pabrower@icloud.com>
2018-11-09 00:12:07 -0800

    completed user sign in and out

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,6 +5,9 @@ const userController = require("../controllers/userController");
 const User = require('../../src/db/models').User;
 
 router.get("/users/signup", userController.signUp);
+router.get("/users/sign_in", userController.signInForm);
+router.post("/users/sign_in", validation.validateUsers, userController.signIn);
+router.get("/users/sign_out", userController.signOut);
 router.post("/users", validation.validateUsers, userController.create);
 
 module.exports = router;
--- src/routes/validation.js
+++ src/routes/validation.js
@@ -3,7 +3,7 @@ module.exports = {
     validateUsers(req, res, next) {
         if (req.method === "POST") {
             req.checkBody("email", "must be valid").isEmail();
-            req.checkBody("name", "must be at least 4 characters in length").isLength({
+            req.checkBody("name", "must be at least 3 characters in length").isLength({
                 min: 4
             })
             req.checkBody("password", "must be at least 6 characters in length").isLength({
